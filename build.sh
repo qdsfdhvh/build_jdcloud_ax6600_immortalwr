fad894de9133de4de8f66e0816f62a4415b53c3f
seiko <seiko_des@outlook.com>
2024-12-07 12:50:58 +0800

    upload build.log

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@ if [ ! -d "immortalwrt" ]; then
     git clone https://github.com/VIKINGYFY/immortalwrt --single-branch --depth 1 || exit 1
 fi
 
+ROOT_DIR=$PWD
+LOG_FILE=$PWD/build.log
+rm -f $LOG_FILE
+
 cp -f ax6600.config immortalwrt/.config
 cd immortalwrt
 
@@ -9,4 +13,4 @@ cd immortalwrt
 ./scripts/feeds install -a
 
 make download V=s
-make -j$(nproc) V=s
+make -j$(nproc) V=s 2>&1 | tee $LOG_FILE
