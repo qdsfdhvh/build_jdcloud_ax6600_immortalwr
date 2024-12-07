82f1b54d773595274032b258bd6aef98118dc8ad
seiko <seiko_des@outlook.com>
2024-12-07 15:03:18 +0800

    update build

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
-if [ ! -d "immortalwrt" ]; then
-    git clone https://github.com/VIKINGYFY/immortalwrt --single-branch --depth 1 || exit 1
-fi
-
 ROOT_DIR=$PWD
-LOG_FILE=$PWD/build.log
+LOG_FILE=$ROOT_DIR/build.log
 rm -f $LOG_FILE
+
+# 确保管道中的错误能被捕获
+set -e
+set -o pipefail
+
+if [ ! -d "immortalwrt" ]; then
+    echo "Cloning immortalwrt..."
+    git clone https://github.com/VIKINGYFY/immortalwrt --single-branch --depth 1 || {
+        echo "Failed to clone immortalwrt"
+        exit 1
+    }
+fi
 
 cp -f ax6600.config immortalwrt/.config
 cd immortalwrt
@@ -12,5 +20,7 @@ cd immortalwrt
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-make download V=s
-make -j$(nproc) V=s 2>&1 | tee $LOG_FILE
+make clean
+make download V=s 2>&1 | tee "$LOG_FILE"
+make -j "$(nproc)" V=s 2>&1 | tee "$LOG_FILE"
+echo "Build Done!"
