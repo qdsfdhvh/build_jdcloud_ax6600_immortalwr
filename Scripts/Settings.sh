17be347f880fc5997f30df41db4b18a1e488e6c4
seiko <seiko_des@outlook.com>
2024-12-08 11:39:27 +0800

    update build ci

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -36,15 +36,3 @@ echo "CONFIG_PACKAGE_luci=y" >> ./.config
 echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
-
-#手动调整的插件
-if [ -n "$WRT_PACKAGE" ]; then
-	echo "$WRT_PACKAGE" >> ./.config
-fi
-
-#高通平台调整
-if [[ $WRT_TARGET == *"IPQ"* ]]; then
-	#取消nss相关feed
-	echo "CONFIG_FEED_nss_packages=n" >> ./.config
-	echo "CONFIG_FEED_sqm_scripts_nss=n" >> ./.config
-fi
