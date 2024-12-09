77e09b53d90ea9655ce3f59e2aea9942ada90441
seiko <seiko_des@outlook.com>
2024-12-09 10:13:20 +0800

    update build

--- build.sh
+++ build.sh
@@ -1,22 +1,27 @@
+sudo apt -y update
+sudo apt -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 unzip zlib1g-dev lib32gcc-s1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsync
+sudo bash -c 'bash <(curl -sL https://build-scripts.immortalwrt.org/init_build_environment.sh)'
+
 ROOT_DIR=$PWD
 LOG_FILE=$ROOT_DIR/build.log
 rm -f $LOG_FILE
 
-if [ ! -d "immortalwrt" ]; then
-    echo "Cloning immortalwrt..."
-    git clone https://github.com/VIKINGYFY/immortalwrt --single-branch --depth 1 || {
+if [ ! -d "wrt/" ]; then
+    echo "Cloning wrt..."
+    git clone https://github.com/VIKINGYFY/immortalwrt --single-branch --depth 1 wrt/ || {
         echo "Failed to clone immortalwrt"
         exit 1
     }
 fi
+cd wrt/
 
-cp -f ax6600.config immortalwrt/.config
-cd immortalwrt
+cp -f $ROOT_DIR/ax6600.config .config
+make defconfig
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-make clean
 make download -j$(nproc) 2>&1 | tee "$LOG_FILE"
 make -j$(nproc) V=s 2>&1 | tee "$LOG_FILE"
+
 echo "Build Done!"
