22ed3ea121dbb0982f0e1678b4ed7b31fcd50276
seiko <seiko_des@outlook.com>
2024-12-12 09:08:17 +0800

    update build ci

--- build.sh
+++ build.sh
@@ -1,5 +1,43 @@
 sudo apt -y update
-sudo apt -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 unzip zlib1g-dev lib32gcc-s1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsync
+sudo apt -y install git curl wget\
+    build-essential \
+    asciidoc \
+    binutils \
+    bzip2 \
+    gawk \
+    gettext \
+    libncurses5-dev \
+    libz-dev \
+    patch \
+    python3 \
+    unzip \
+    zlib1g-dev \
+    lib32gcc-s1 \
+    libc6-dev-i386 \
+    subversion \
+    flex \
+    uglifyjs \
+    gcc-multilib \
+    p7zip-full\
+    msmtp \
+    libssl-dev \
+    texinfo \
+    libglib2.0-dev \
+    xmlto \
+    qemu-utils \
+    upx \
+    libelf-dev \
+    autoconf \
+    automake \
+    libtool \
+    autopoint\
+    device-tree-compiler \
+    g++-multilib \
+    antlr3 \
+    gperf \
+    swig \
+    rsync \
+    genisoimage
 sudo bash -c 'bash <(curl -sL https://build-scripts.immortalwrt.org/init_build_environment.sh)'
 
 ROOT_DIR=$PWD
