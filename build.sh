2fcfb50fb7627a454106a77963a2cde3e94b96eb
seiko <seiko_des@outlook.com>
2024-12-12 11:37:26 +0800

    update build ci

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # 初始化构建环境
-sudo bash -c 'bash <(curl -sL https://build-scripts.immortalwrt.org/init_build_environment.sh)'
+# sudo bash -c 'bash <(curl -sL https://build-scripts.immortalwrt.org/init_build_environment.sh)'
 
 # 更新并安装依赖 https://openwrt.org/docs/guide-developer/toolchain/install-buildsystem#debianubuntumint
-sudo apt update
-sudo apt install build-essential clang flex bison g++ gawk \
+sudo apt update -y
+sudo apt install -y build-essential clang flex bison g++ gawk \
     gcc-multilib g++-multilib gettext git libncurses5-dev libssl-dev \
     python3-setuptools rsync swig unzip zlib1g-dev file wget
+sudo apt install -y $(curl -fsSL https://raw.githubusercontent.com/nantayo/My-Pkg/master/2305)
+sudo apt clean
 
 ROOT_DIR=$PWD
 LOG_FILE=$ROOT_DIR/build.log
@@ -31,4 +33,4 @@ cp -f $ROOT_DIR/ax6600.config .config
 
 # 下载和编译
 make download -j$(nproc) 2>&1 | tee "$LOG_FILE"
-make -j$(nproc) V=s 2>&1 | tee "$LOG_FILE"
+make -j1 V=s 2>&1 | tee "$LOG_FILE"
